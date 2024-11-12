44c970d155c811af4bcc6a4a64be06057951886c
bhaskar-thakur <143820431+bhaskar-thakur@users.noreply.github.com>
2024-11-12 12:17:01 +0000

    done

--- basics_01/03_conversionOperation.js
+++ basics_01/03_conversionOperation.js
@@ -12,3 +12,19 @@ console.log(valueInNumber)
 // "33cjbs" =>NaN
 //  true => 1;false =>0
 
+
+
+
+
+// ********************* Operations *********************
+
+let value = 3
+let negvalue=-value
+console.log(negvalue)
+
+
+
+let str1 = "hello"
+let str2="there"
+let str3=str1+str2
+console.log(str3) 
---
+++ basics_01/memory.js
@@ -0,0 +1,25 @@
+//  Stack(Primitive),Heap(Non-Primitive)
+
+
+let myName="Bhaskar"
+
+let anotherName= myName
+anotherName="Thakur"
+console.log(myName)
+console.log(anotherName);
+
+
+let userOne ={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo =userOne
+
+userTwo.email="abcd@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+
+
